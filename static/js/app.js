0656fb6efa65a4946003a9b5bea3fe183d80f1c0
Taravat <eau0715362@gmail.com>
2021-06-27 16:46:06 -0700

    app.js changed.

--- static/js/app.js
+++ static/js/app.js
@@ -60,7 +60,7 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    //for (var filterValues in filters) {
+    for (var filterValues in filters) {
       filteredData = filteredData.filter(r => r[filterValues] === filters[filterValues]);
     }
 
